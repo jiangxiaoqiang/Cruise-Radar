616bf099e0b69da882a74124969ba0e3de1ea7a4
jiang qiang <jiangtingqiang@gmail.com>
2021-04-04 09:10:33 +0800

    [feature] 新增post请求添加channel

--- src/js/popup/index.js
+++ src/js/popup/index.js
@@ -6,6 +6,53 @@ import aboutIcon from '../../svg/about.svg';
 import MD5 from 'md5.js';
 let config;
 
+function fetchAuthToken(username,password){
+    const req = new XMLHttpRequest();
+    const baseUrl = "http://121.196.199.223:11014/post/user/login";
+    const urlParams = {
+        "phone": username,
+        "password": password,
+    };
+
+    req.open("POST", baseUrl, true);
+    req.setRequestHeader("Content-type", "application/json");
+    req.send(JSON.stringify(urlParams));
+
+    req.onreadystatechange = function() {
+        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+            var body = req.response;
+            var res = JSON.parse(body);
+            if(res && res.result && res.result.token){
+                var authToken = res.result.token;
+                console.log("token1:" + authToken);
+                chrome.storage.local.set({
+                    cruiseToken: authToken
+                });
+            }
+        }
+    }
+}
+
+function getCachedAuthToken(times){
+    var cachedToken = "";
+    if(times > 3){
+        return "";
+    }
+    chrome.storage.local.get('cruiseToken', (result) => {
+        if (result.cruiseToken) {
+            console.log("tokenstringify:" + JSON.stringify(result));
+            cachedToken = JSON.stringify(result);
+        } else {
+            console.log("token:wwwww");
+            fetchAuthToken("+8615683761628","12345678");
+            ++times;
+            getCachedAuthToken(times);
+        }
+    });
+    console.log("cachedToken:" + cachedToken);
+    return cachedToken;
+}
+
 function generateList(type, list) {
     let result = '';
     if (list && list.length) {
@@ -28,7 +75,8 @@ function generateList(type, list) {
                 ${
                     item.isDocs
                         ? `<a href="${url}" class="rss-action">文档</a>`
-                        : `<div class="rss-action rss-copy" data-clipboard-text="${url}">复制</div>
+                        : `<div class="rss-action rss-copy" data-clipboard-text="${url}">复制</div>`
+                }
                 ${
                     config.submitto.ttrss && config.submitto.ttrssDomain
                         ? `<a href="${config.submitto.ttrssDomain.replace(/\/$/, '')}/public.php?op=subscribe&feed_url=${encodeURI(url)}" class="rss-action rss-submitto-ttrss">订阅到 TTRSS</a>`
@@ -44,8 +92,12 @@ function generateList(type, list) {
                         ? `<a href="${config.submitto.freshrssDomain.replace(/\/$/, '')}/i/?c=feed&a=add&url_rss=${encodeURI(url)}" class="rss-action rss-submitto-freshrss">订阅到 FreshRSS</a>`
                         : ''
                 }
-                ${config.submitto.feedly ? `<a href="http://121.196.199.223:11014/post/sub/source/temp/add?url=${encodeURI(url)}&userId=7" class="rss-action rss-submitto-feedly">订阅到 Cruise</a>` : ''}
-                }
+                ${
+                    config.submitto.feedly ? `<a href="http://121.196.199.223:11014/post/sub/source/temp/add?url=${encodeURI(url)}&userId=7" class="rss-action rss-submitto-feedly">订阅到 Cruise</a>` : ''
+                } 
+                ${
+                    config.submitto.feedly ? `<input url="${encodeURI(url)}" type="submit" value="添加" class="rss-action rss-submitto-feedly"></input>` : ''
+                }      
             </li>
             `;
         });
@@ -93,6 +145,7 @@ chrome.tabs.query(
                         });
                     });
 
+                    // handle a label click event
                     document.querySelectorAll('a').forEach((ele) => {
                         ele.addEventListener('click', (e) => {
                             e.preventDefault();
@@ -100,6 +153,35 @@ chrome.tabs.query(
                                 url: ele.getAttribute('href'),
                             });
                             window.close();
+                        });
+                    });
+
+                    document.querySelectorAll('input').forEach((e)=>{
+                        e.addEventListener('click',(innerEvent) =>{
+                            innerEvent.preventDefault();
+                            getCachedAuthToken(0);
+                            
+                            chrome.storage.local.get('cruiseToken', (result) => {
+                                var rssUrl = e.getAttribute('url');
+                                const req = new XMLHttpRequest();
+                                const baseUrl = "http://121.196.199.223:11014/post/sub/source/add";
+                                const urlParams ={
+                                    subUrl:rssUrl
+                                }; 
+                    
+                                req.open("POST", baseUrl, true);
+                                req.setRequestHeader("token",result.cruiseToken);
+                                req.setRequestHeader("Content-type", "application/json");
+                                req.send(JSON.stringify(urlParams));
+                                
+                                req.onreadystatechange = function() {
+                                    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+                                        var body = req.response;
+                                        console.log(body);
+                                    }
+                                }
+                            });
+                              
                         });
                     });
                 }
